537493e1ec5ce3f7fac295a4438126c6e158790a
muzakparov <muzakparov@gmail.com>
2019-02-24 01:57:27 +0300

    s

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-	res.send("s");
-	// res.json([
-	// 	{
-	// 		action: "talk",
-	// 		text: "Connecting you now."
-	// 	}
-	// ]);
+	res.json([
+		{
+			action: "talk",
+			text: "Connecting you now."
+		}
+	]);
 	// res.render('index', { title: 'Express' });
 });
 
